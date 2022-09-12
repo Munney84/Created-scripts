d0b09efe7ae8710696a27a7a0fa5dfd19c601dd0
clarence <clarencemunn2@yahoo.com>
2022-09-12 16:48:04 -0400

    added file_info.sh

---
+++ file_info.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+# Title	        :File Information
+# Description	:This script will display all information of a file in the 
+#                   current working directory.
+# Author	    :Clarence Munn
+# Date		    :08-15-2022
+# Version	    : 1
+# Usage		    : ./file_info.sh
+# Notes		    :
+#=======================================================================
+
+ls -l
